f3a29afccd7d7e4df2247a021885f63935290cde
cobentil02 <carlosobeng9@gmail.com>
2023-10-20 13:18:16 +0100

    added models folder and it content

---
+++ Models/Log.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.Models
+{
+    public class UserCredentials
+    {
+        [PrimaryKey]
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+public class Recipe
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public string Description { get; set; }
+    public string Author { get; set; }
+    public List<string> Ingredients { get; set; }
+    public List<string> Method { get; set; }
+    public bool IsFavorite { get; set; }
+
+
+}
